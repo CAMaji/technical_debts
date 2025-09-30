6c00477d93b4c0fb18f5d30288cd9d25a11b13c5
Simon St-Andre <simonstandre@gmail.com>
2025-09-30 00:29:17 -0400

    Fix log and add hot reload

--- web/app.py
+++ web/app.py
@@ -3,18 +3,16 @@ from flask import request, render_template, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from config import BaseConfig
 
-
 app = Flask(__name__)
 app.config.from_object(BaseConfig)
 db = SQLAlchemy(app)
 
-
 from models import *
-
 
 @app.route('/', methods=['GET'])
 def index():
     posts = Post.query.order_by(Post.date_posted.desc()).all()
+    print('here')
     return render_template('index.html', posts=posts)
 
 @app.route('/submit', methods=['POST'])
@@ -24,7 +22,6 @@ def submit():
     db.session.add(post)
     db.session.commit()
     return redirect(url_for('index'))
-
 
 if __name__ == '__main__':
     app.run()
